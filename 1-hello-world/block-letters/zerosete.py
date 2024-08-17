579c6d4efec1d2a8a2d358642173bc02f8f1ad74
ZerosetePy <lima.casas@gmail.com>
2024-08-16 21:15:11 -0300

    Create zerosete.py

---
+++ 1-hello-world/block-letters/zerosete.py
@@ -0,0 +1,21 @@
+#Rodrigo Casas
+#I have central heterochromia in both my eyes
+first_letter = """
+RRRR
+R   R
+R   R
+RRRR
+R R
+R  R
+R   R"""
+all_letters = """
+RRRR   CCC
+R   R C   C
+R   R C
+RRRR  C
+R R   C
+R  R  C   C
+R   R  CCC"""
+
+print(first_letter)
+print(all_letters)
